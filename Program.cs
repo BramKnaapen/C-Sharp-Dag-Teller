257669f6f86a3a5bbe47b1cb79bf05ff89d90de3
BramKnaapen <159915417+BramKnaapen@users.noreply.github.com>
2024-06-07 13:38:52 +0200

    Add(Nieuw Project C#)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Wat is het jaar?: ");
+        int jaar = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wat is het maandnummer?: ");
+        int maand = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wat is de dag?: ");
+        int dag = int.Parse(Console.ReadLine());
+
+        DateTime ingevuldeDatum = new DateTime(jaar, maand, dag);
+        DateTime nuDatum = DateTime.Now;
+        TimeSpan verschil = nuDatum - ingevuldeDatum;
+        Console.WriteLine(verschil.Days);
+    }
+}
